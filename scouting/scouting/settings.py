102a3e7e69648a54f0c1577fd27189ff8e7a15ea
nfoert <nofoert@gmail.com>
2024-10-19 10:59:53 -0400

    Use environment variables for settings

--- scouting/scouting/settings.py
+++ scouting/scouting/settings.py
@@ -13,20 +13,36 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+import environ
+
+env = environ.Env(interpolate=True,
+    DEBUG=(bool, False),
+    SECRET_KEY=(
+        str,
+        "django-insecure-#8&%8a-xu)r#tv0ax8csheta#%77ir2#5m!aollbu=92r7ltp(",
+    ),
+    DJANGO_ALLOWED_HOSTS=(list, ["localhost", "127.0.0.1"]),
+    ADMIN_PATH=(str, "admin/"),
+    DATABASE_URL=(str, "sqlite:///db.sqlite3")
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(BASE_DIR / ".." / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#8&%8a-xu)r#tv0ax8csheta#%77ir2#5m!aollbu=92r7ltp('
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS")
+
+ADMIN_PATH = env("ADMIN_PATH")
 
 
 # Application definition
@@ -76,10 +92,7 @@ WSGI_APPLICATION = 'scouting.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": env.db(),
 }
 
 
