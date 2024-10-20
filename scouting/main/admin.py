94be1bcf8b22dd1779785a201addd83a6529a271
nfoert <nofoert@gmail.com>
2024-10-20 15:27:28 -0400

    feat: Create models

--- scouting/main/admin.py
+++ scouting/main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from main.models import SeasonFields, Data
 
-# Register your models here.
+admin.site.register(SeasonFields)
+admin.site.register(Data)
--- scouting/main/models.py
+++ scouting/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+YEARS = [("2024", "2024")]
+
+# Stores the fields needed for scouting each season, in JSON
+class SeasonFields(models.Model):
+    year = models.IntegerField(choices=YEARS)
+    data = models.JSONField(default=dict, blank=True)
+
+    def __str__(self):
+        return self.year
+
+    class Meta:
+        verbose_name_plural = "Season Fields"
+
+class Data(models.Model):
+    year = models.IntegerField(choices=YEARS)
+    event = models.CharField(max_length=999)
+    event_code = models.CharField(max_length=99)
+    data = models.JSONField(default=dict, blank=True)
+
+    def __str__(self):
+        return f"Data from {self.event} in {self.year}"
+
+    class Meta:
+        verbose_name_plural = "Data"
