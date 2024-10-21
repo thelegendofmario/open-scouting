ee7c2e9460ed3cd6a20b7ddf8e8c5de4a76d8c48
nfoert <nofoert@gmail.com>
2024-10-21 13:50:07 -0400

    feat: Update season_fields to better support sections Pass this data to the client

--- scouting/main/views.py
+++ scouting/main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponse
 from django.conf import settings
 from . import season_fields
 
+import json
+
 def index(request):
     context = {
         "SERVER_IP": settings.SERVER_IP,
@@ -14,7 +16,7 @@ def contribute(request):
     context = {
         "SERVER_IP": settings.SERVER_IP,
         "TBA_API_KEY": settings.TBA_API_KEY,
-        "season_fields": season_fields.crescendo
+        "season_fields": json.dumps(season_fields.crescendo)
     }
 
     return render(request, "contribute.html", context)
