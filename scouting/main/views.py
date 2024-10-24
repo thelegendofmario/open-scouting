cf42021a5eca20b06a8deabb9db402d989ef6caf
nfoert <nofoert@gmail.com>
2024-10-24 15:04:51 -0400

    feat: Save the collected data to the django session and display on the contributing page

--- scouting/main/views.py
+++ scouting/main/views.py
@@ -13,12 +13,17 @@ def index(request):
     return render(request, "index.html", context)
 
 def contribute(request):
+    request.session["username"] = request.GET.get("username", "unknown")
+    request.session["event_name"] = request.GET.get("event_name", "unknown")
+    request.session["event_code"] = request.GET.get("event_code", "unknown")
+
     context = {
         "SERVER_IP": settings.SERVER_IP,
         "TBA_API_KEY": settings.TBA_API_KEY,
         "season_fields": json.dumps(season_fields.crescendo),
-        "event": "unknown",
-        "username": "unknown"
+        "username": request.GET.get("username", "unknown"),
+        "event_name": request.GET.get("event_name", "unknown"),
+        "event_code": request.GET.get("event_code", "unknown")
     }
 
     return render(request, "contribute.html", context)
