1a3583f69422570e0c0081641d7f3fddedb05371
nfoert <nofoert@gmail.com>
2024-11-29 21:26:29 -0500

    feat: Make service worker network first instead of cache first

--- scouting/frontend/sw.js
+++ scouting/frontend/sw.js
@@ -2,83 +2,80 @@ const addResourcesToCache = async (resources) => {
     const cache = await caches.open("v1");
     await cache.addAll(resources);
     console.log("Cached many resources");
-  };
-  
-  const putInCache = async (request, response) => {
+};
+
+const putInCache = async (request, response) => {
     const cache = await caches.open("v1");
     await cache.put(request, response);
     console.log("Cached: ", request.url);
-  };
-  
-  const cacheFirst = async ({ request, preloadResponsePromise }) => {
-    // First try to get the resource from the cache
-    const responseFromCache = await caches.match(request);
-    if (responseFromCache) {
-      console.log("Found in cache: ", request.url);
-      return responseFromCache;
-    }
-  
-    // Next try to use (and cache) the preloaded response, if it's there
-    const preloadResponse = await preloadResponsePromise;
-    if (preloadResponse) {
-      console.log("Using preload response", preloadResponse);
-      putInCache(request, preloadResponse.clone());
-      return preloadResponse;
-    }
-  
-    // Next try to get the resource from the network
-    try {
-      const responseFromNetwork = await fetch(request);
-      // response may be used only once
-      // we need to save clone to put one copy in cache
-      // and serve second one
-      putInCache(request, responseFromNetwork.clone());
-      return responseFromNetwork;
-    } catch (error) {
+};
 
-      // when even the fallback response is not available,
-      // there is nothing we can do, but we must always
-      // return a Response object
-      return new Response("Network error happened", {
-        status: 408,
-        headers: { "Content-Type": "text/plain" },
-      });
+const cacheFirst = async ({ request, preloadResponsePromise }) => {
+    // First try to get the resource from the network
+    try {
+        const responseFromNetwork = await fetch(request);
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        if (responseFromNetwork) {
+            putInCache(request, responseFromNetwork.clone());
+            return responseFromNetwork;
+        }
+
+        // Next try to use (and cache) the preloaded response, if it's there
+        const preloadResponse = await preloadResponsePromise;
+        if (preloadResponse) {
+            console.log("Using preload response", preloadResponse);
+            putInCache(request, preloadResponse.clone());
+            return preloadResponse;
+        }
+
+        // Finally try to get the resource from cache
+        const responseFromCache = await caches.match(request);
+        if (responseFromCache) {
+            console.log("Found in cache: ", request.url);
+            return responseFromCache;
+        }
+    } catch (error) {
+        return new Response("Network error happened", {
+            status: 408,
+            headers: { "Content-Type": "text/plain" },
+        });
     }
-  };
-  
-  // Enable navigation preload
-  const enableNavigationPreload = async () => {
+};
+
+// Enable navigation preload
+const enableNavigationPreload = async () => {
     if (self.registration.navigationPreload) {
-      await self.registration.navigationPreload.enable();
+        await self.registration.navigationPreload.enable();
     }
-  };
-  
-  self.addEventListener("activate", (event) => {
+};
+
+self.addEventListener("activate", (event) => {
     event.waitUntil(enableNavigationPreload());
-  });
-  
-  self.addEventListener("install", (event) => {
+});
+
+self.addEventListener("install", (event) => {
     event.waitUntil(
-      addResourcesToCache([
-        "/",
-        "/contribute",
-        "/data",
-        "/static/main/scripts/app.js",
-        "/static/main/styles.css",
-        "/static/main/images/favicon.ico",
-        "/static/main/images/icon_rounded.png",
-        "/static/main/images/logo_dark.png",
-        "/static/main/images/logo_light.png",
-      ]),
+        addResourcesToCache([
+            "/",
+            "/contribute",
+            "/data",
+            "/static/main/scripts/app.js",
+            "/static/main/styles.css",
+            "/static/main/images/favicon.ico",
+            "/static/main/images/icon_rounded.png",
+            "/static/main/images/logo_dark.png",
+            "/static/main/images/logo_light.png",
+        ]),
     );
-  });
-  
-  self.addEventListener("fetch", (event) => {
+});
+
+self.addEventListener("fetch", (event) => {
     event.respondWith(
-      cacheFirst({
-        request: event.request,
-        preloadResponsePromise: event.preloadResponse,
-      }),
+        cacheFirst({
+            request: event.request,
+            preloadResponsePromise: event.preloadResponse,
+        }),
     );
-  });
-  
+});
