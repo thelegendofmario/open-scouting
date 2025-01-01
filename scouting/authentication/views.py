6c003ccd5e75648c1fd9922095536a824edbe2cc
nfoert <nofoert@gmail.com>
2025-01-01 18:02:46 -0500

    ui: Add a section to the create account UI for entering a verification code

--- scouting/authentication/views.py
+++ scouting/authentication/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, redirect
+from django.conf import settings
 
 
 def auth(request):
-    return render(request, "authentication.html")
+    context = {
+        "SERVER_IP": settings.SERVER_IP,
+        "TBA_API_KEY": settings.TBA_API_KEY,
+        "SERVER_MESSAGE": settings.SERVER_MESSAGE,
+        "EMAIL_HOST_USER": settings.EMAIL_HOST_USER,
+    }
+
+    return render(request, "authentication.html", context)
