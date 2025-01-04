7d95b2f9aa85c1f63655eb70e506f0874da34b70
nfoert <nofoert@gmail.com>
2025-01-04 09:51:21 -0500

    feat: Add urls for authentication views

--- scouting/authentication/urls.py
+++ scouting/authentication/urls.py
@@ -4,4 +4,18 @@ from . import views
 
 urlpatterns = [
     path("", views.auth, name="auth"),
+    path("sign_in", views.sign_in, name="sign_in"),
+    path("forgot_password", views.forgot_password, name="forgot_password"),
+    path("change_password", views.change_password, name="change_password"),
+    path(
+        "send_verification_code",
+        views.send_verification_code,
+        name="send_verification_code",
+    ),
+    path(
+        "check_verification_code",
+        views.check_verification_code,
+        name="check_verification_code",
+    ),
+    path("create_account", views.create_account, name="create_account"),
 ]
