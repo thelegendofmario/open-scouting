bf90015185532652309b7339e5c0067c63a05f37
nfoert <nofoert@gmail.com>
2025-01-23 15:32:39 -0500

    test: Add tests for the authentication page

--- scouting/authentication/tests.py
+++ scouting/authentication/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from authentication.models import User, Profile
 
-# Create your tests here.
+
+class AuthenticationPageTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        user = User.objects.create_user("test", "test", "test")
+        user.save()
+
+        profile = Profile(user=user, display_name="test", team_number="1234")
+        profile.save()
+
+    def test_authentication_anonymous(self):
+        response = self.client.get("/authentication/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_authentication_authenticated(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get("/authentication/")
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, "/")
