21ab67c9aee18ed231dfcee75f0e4e9310863033
nfoert <nofoert@gmail.com>
2025-01-24 15:19:43 -0500

    fix: Authentication status isn't updated on index page due to caching #46

--- scouting/main/urls.py
+++ scouting/main/urls.py
@@ -12,6 +12,14 @@ urlpatterns = [
     path("get_custom_events", views.get_custom_events, name="get_custom_events"),
     path("create_custom_event", views.create_custom_event, name="create_custom_event"),
     path("get_year_data", views.get_year_data, name="get_year_data"),
-    path("check_local_backup_reports", views.check_local_backup_reports, name="check_local_backup_reports"),
-    path("upload_offline_reports", views.upload_offline_reports, name="upload_offline_reports")
+    path(
+        "check_local_backup_reports",
+        views.check_local_backup_reports,
+        name="check_local_backup_reports",
+    ),
+    path(
+        "upload_offline_reports",
+        views.upload_offline_reports,
+        name="upload_offline_reports",
+    ),
 ]
