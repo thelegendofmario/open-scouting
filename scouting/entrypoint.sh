6a15082409cf98b4eb3a401f2df6aabfb6f29946
nfoert <nofoert@gmail.com>
2025-02-01 09:43:52 -0500

    build: Forgot to create superuser when server begins

--- scouting/entrypoint.sh
+++ scouting/entrypoint.sh
@@ -13,8 +13,27 @@ fi
 
 # Run migrations
 python manage.py migrate
+
 # Collect static files
 python manage.py collectstatic --noinput
+
+# Create super user
+echo "Checking if a superuser already exists..."
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(is_superuser=True).exists():
+    import os
+    username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
+    email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+    password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'password123')
+    
+    User.objects.create_superuser(username, email, password)
+    print("Superuser created successfully.")
+else:
+    print("Superuser already exists. Skipping creation.")
+EOF
 
 exec "$@"
 
